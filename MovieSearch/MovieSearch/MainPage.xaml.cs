899dddfeea4bb0e2c133d9ab26ed3addecd2701e
henquesz <henquesvinicius@gmail.com>
2022-04-16 14:32:01 -0300

    first commit of development

---
+++ MovieSearch/MovieSearch/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MovieSearch
+{
+    public partial class MainPage : ContentPage
+    {
+        Class1 apicall = new Class1();
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btn_pesquisar_Clicked(object sender, EventArgs e)
+        {
+            string url = "http://www.omdbapi.com/?s=" + lbl_filme.Text;
+            
+        }
+
+        private void btn_limpar_Clicked(object sender, EventArgs e)
+        {
+            lbl_filme.Text = "";
+        }
+    }
+}
